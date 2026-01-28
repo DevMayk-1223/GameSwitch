08e356549cb25e305f88ba50a40e3c254a847504
DevMayk <soaresmayk90@gmail.com>
2026-01-27 23:53:14 -0300

    Aplicação do menu admin e algumas alterações no idioma do programa

--- src/GameSwitch.java
+++ src/GameSwitch.java
@@ -1,47 +1,44 @@
+
 import java.util.Scanner;
 
   public class GameSwitch {
 
-      public static void mostrarMenu(){
-          System.out.println("Welcome to our program, where do you want to go?");
-          System.out.println("1 - Open Game");
-          System.out.println("2 - Open Settings");
-          System.out.println("3 - Help");
-          System.out.println("4 - Exit");
+      public static void mostrarMenu() {
+          System.out.println("Bem - Vindo ao nosso programa, para onde iremos?");
+          System.out.println("1 - Abrir jogos");
+          System.out.println("2 - Configurações");
+          System.out.println("3 - Ajuda");
+          System.out.println("4- Sair");
       }
 
       public static int lerEscolhaDoUsuario(Scanner pedido) {
           int opcao;
 
           while (true) {
-              System.out.println("Choose an optioon: ");
+              System.out.println("Escolha uma opcão ");
               opcao = pedido.nextInt();
 
               if (opcao >= 1 && opcao <= 4) {
                   return opcao;
               }
-              System.out.println("Invalid, try again!");
+              System.out.println("Invalido, tente novamente");
           }
 
       }
 
-      public static void abrirSettings() {
-          System.out.println("Entering our settings, wait...");
-
-      }
 
       public static void abrirHelp() {
-          System.out.println("Entering our help space...");
+          System.out.println("Entrando na aba de ajuda...");
       }
 
-      public static void menuGames(){
+      public static void menuGames() {
 
-          System.out.println("Entering our games space...");
-          System.out.println("----- GAMES -----");
-          System.out.println("1 - Cont Numbers");
-          System.out.println("2 - Sum numbers");
-          System.out.println("3 - Odd or even");
-          System.out.println("4 - Rerturn to menu");
+          System.out.println("Entrando no menu de jogos...");
+          System.out.println("----- Jogos -----");
+          System.out.println("1 - Contador de numeros");
+          System.out.println("2 - Soma de numeros");
+          System.out.println("3 -Impar ou par ");
+          System.out.println("4 - Retornar ao menu");
 
       }
 
@@ -58,9 +55,9 @@ import java.util.Scanner;
               int choose;
               int cont = 0;
 
-              System.out.println("---- Welcome to the game, let's start :) ----");
-              System.out.println("We will count from 1 to the number you choose");
-              System.out.println("Choose the number:");
+              System.out.println("----Bem - Vindo ao jogo, vamos iniciar :) ----");
+              System.out.println("Nós vasmos contar de 1 até o numero que você escolher");
+              System.out.println("Escolha um numero:");
 
               choose = pedido.nextInt();
 
@@ -69,19 +66,18 @@ import java.util.Scanner;
                   System.out.println(cont);
               }
 
-              System.out.println("We finished our count, congratulations ;)");
-          }
-          else if (game == 2){
+              System.out.println("Terminamos a nossa conta, parabéns ;)");
+          } else if (game == 2) {
 
               int choose;
               int cont = 0;
               int sum = 0;
 
-              System.out.println("Chose an number: ");
+              System.out.println("Escolha um numero ");
 
               choose = pedido.nextInt();
 
-              while(cont < choose){
+              while (cont < choose) {
                   cont++;
                   sum = sum + cont;
                   System.out.println(cont);
@@ -89,18 +85,18 @@ import java.util.Scanner;
 
               int resultado = sum;
 
-              System.out.println("Finishe ! The result: " + resultado );
+              System.out.println("Terminamos!! resultado: " + resultado);
 
-          } else if (game == 3){
+          } else if (game == 3) {
               jogoParOuImpar(pedido);
 
           } else if (game == 4) {
-              System.out.println("Returning...");
+              System.out.println("Voltando...");
               return;
           }
       }
 
-      public static void jogoParOuImpar(Scanner pedido){
+      public static void jogoParOuImpar(Scanner pedido) {
 
           int escolha;
 
@@ -117,28 +113,48 @@ import java.util.Scanner;
 
       }
 
-      public static void mostrarBoasVindas(){
+      public static void mostrarBoasVindas() {
 
-          System.out.println("////////////WELCOME :)\\\\\\\\\\\\\\");
+          System.out.println("////////////Bem - Vindo :)\\\\\\\\\\\\\\");
 
       }
 
-      public static void exitGame(){
-          System.out.println("Exiting...");
+      public static void exitGame() {
+          System.out.println("Saindo...");
           System.exit(0);
       }
 
-      public static void executarOpcao(int opcao, Scanner pedido){
+      public static void executarMenuAdmin(int opcao, Scanner pedido) {
 
-          switch (opcao){
+          switch (opcao) {
+
+              case 1:
+                  System.out.println("Mostrando Usuário...");
+                  break;
+
+              case 2:
+                  System.out.println("Criando Usuário...");
+                  break;
+
+              case 3:
+                  System.out.println("Saindo do menu Admin...");
+                  break;
+
+              default:
+                  System.out.println("Esta opcão não existe :(");
+          }
+      }
+
+      public static void executarOpcao(int opcao, Scanner pedido) {
+
+          switch (opcao) {
 
               case 1:
                   abrirGames(pedido);
                   break;
 
               case 2:
-
-                  abrirSettings();
+                  abrirSettings(pedido);
                   break;
 
               case 3:
@@ -149,13 +165,59 @@ import java.util.Scanner;
                   exitGame();
                   break;
 
+              case 5:
+                  mostrarMenuAdmin(pedido);
+                  break;
+
               default:
-                  System.out.println("This option don't exist");
+                  System.out.println("Esta opcão não existe :(");
           }
       }
 
+      public static void abrirSettings(Scanner pedido){
 
-      public static void main(String[] args) {
+          while(true) {
+              System.out.println("1- Abrir menu do admin");
+              System.out.println("2- Voltar ao menu");
+
+              int escolha = lerEscolhaDoUsuario(pedido);
+
+              switch (escolha) {
+
+                  case 1:
+                      mostrarMenuAdmin(pedido);
+                      break;
+
+                  case 2:
+                      System.out.println("Votando...");
+                      return;
+
+                  default:
+                      System.out.println("Opção não existente :)");
+              }
+          }
+      }
+
+      public static void mostrarMenuAdmin(Scanner pedido) {
+          System.out.println("-----MENU ADMIN-----");
+          System.out.println("1- Ver Usuário");
+          System.out.println("2- Criar usuário");
+          System.out.println("3- Sair do menu admin");
+      }
+      public static void abrirMenuAdmin(Scanner pedido){
+
+          while (true){
+              mostrarMenuAdmin(pedido);
+
+              int escolha = lerEscolhaDoUsuario(pedido);
+              executarMenuAdmin(escolha, pedido);
+
+              if (escolha == 3){
+                  break;
+              }
+          }
+      }
+      public static void main (String[] args) {
           Scanner pedido = new Scanner(System.in);
 
           mostrarBoasVindas();
